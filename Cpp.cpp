24927cab28dff7ca620ae5546edca2375192e9d9
CMU02 <hyeonjun1968@naver.com>
2023-04-14 22:39:16 +0900

    Update Cpp.cpp

--- Cpp.cpp
+++ Cpp.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
 using namespace std;
 
+int Add(int num1, int num2)
+{
+  int Result = num1 + num2;
+  return Result;
+}
+void Question(void)
+{
+  cout<<"You Choice is Two number : "<<endl;
+}
+
 int main()
 {
-  cout<<"I like Pizza"<<"\n";
-  cout<<"It's really good"<<"\n";
+  int num1, num2 = 0;
+  int result = 0;
 
+  Question();
+  cin>>num1>>num2;
+
+  result = Add(num1, num2);
+
+  cout<<result<<endl;
+  
   return 0;
 }
